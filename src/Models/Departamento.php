a90c0053c98728c28dc16f8dae2b30a94c25f4ca
livramateus <livra.matheus@gmail.com>
2021-11-28 17:09:14 -0300

    Criadas as funcionalidades base da página de departamentos

---
+++ src/Models/Departamento.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Src\Models;
+use Src\Config\Db, PDO;
+
+class Departamento {
+
+    private $id;
+    private $nome;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function cadastrar() {
+        $sql = 'INSERT INTO departamentos (nome)
+                                  VALUES (?);';
+
+        $Db = new Db;
+        $cnx = $Db->getConexao();
+
+        $transaction = $cnx->prepare($sql);
+        $transaction->execute([$this->getNome()]);
+    }
+
+    public function deletar() {
+        $sql = 'DELETE
+                  FROM departamentos
+                 WHERE id = ?;';
+        
+        $Db = new Db;
+        $cnx = $Db->getConexao();
+
+        $transaction = $cnx->prepare($sql);
+
+        $transaction->execute([$this->getId()]);
+    }
+
+    public function getDados() {
+        $Db = new Db;
+        
+        $sql= 'SELECT *
+                 FROM departamentos;';
+
+        $res = $Db->getConexao()->query($sql);
+
+        $dados = [];
+
+        while($linha = $res->fetch(PDO::FETCH_ASSOC)) {
+            $dpt = new Departamento();
+
+            $dpt->setId($linha['id']);
+            $dpt->setNome($linha['nome']);
+
+            $dados[] = $dpt;
+        }
+
+        return $dados;
+        
+    }
+
+}
--- src/controllers/Departamento.php
+++ src/controllers/Departamento.php
@@ -1,10 +1,59 @@
 <?php
-
 namespace Src\Controllers;
+
+use Src\Models\Departamento as ModelDepartamento;
+use Src\Views\Departamento as ViewDepartamento;
 
 class Departamento {
 
     public function __construct() {
-        echo 'Construiu o controller de departamento';
+        $ViewDpt = new ViewDepartamento();
+        $this->gerenciaAcoes();
+        
+        $ViewDpt->renderTitulo();
+        $this->mostraDados();
+        $this->mostraFormCadastro();
     }
+
+    private function mostraDados() {
+        $ModelDpt = new ModelDepartamento();
+        $ViewDpt  = new ViewDepartamento();
+
+        $ViewDpt->listarDados($ModelDpt->getDados());
+    }
+
+    private function mostraFormCadastro() {
+        $ViewDpt = new ViewDepartamento();
+        $ViewDpt->montaFormularioCadastro();
+    }
+
+    private function cadastrar() {
+        $ModelDpt = new ModelDepartamento();
+        $ModelDpt->setNome($_POST['nome']);
+
+        $ModelDpt->cadastrar();
+    }
+
+    private function deletar() {
+        $ModelDpt = new ModelDepartamento();
+        $ModelDpt->setId($_POST['reg']);
+
+        $ModelDpt->deletar();
+    }
+
+    private function gerenciaAcoes() {
+        $acao = filter_input(INPUT_GET, 'acao', FILTER_SANITIZE_STRING);
+
+        switch ($acao) {
+            case 'cadastrar':
+                $this->cadastrar();
+                break;
+            case 'deletar':
+                $this->deletar();
+            default:
+                # code...
+                break;
+        }
+    }
+
 }
